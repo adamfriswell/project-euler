28922a4e15410b7c2651a07722d492de75022f7d
Adam Friswell <adamfriswell@googlemail.com>
2020-08-05 23:12:15 +0100

    Menu system

---
+++ Problems/Problem5.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using project_euler.Helpers;
+
+namespace project_euler.Problems {
+    public class Problem5 {
+
+        public void Solve() {
+            Console.WriteLine("Problem 5: " );
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,47 @@
 ﻿using System;
 using project_euler.Problems;
 using project_euler.Helpers;
+
 namespace project_euler {
     class Program {
         static void Main(string[] args) {
 
-            // var p1 = new Problem1();
-            // p1.Solve();
+            bool exit = false;
 
-            // var p2 = new Problem2();
-            // p2.Solve();
+            while (!exit) {
+                Console.Write("Menu: ");
+                string input = Console.ReadLine().ToLower();
 
-            // var p3 = new Problem3();
-            // p3.Solve();
-
-            var helper = new Helper();
-            helper.WritePrimeFactorisation();
+                switch (input) {
+                    case "1":
+                        var p1 = new Problem1();
+                        p1.Solve();
+                        break;
+                    case "2":
+                        var p2 = new Problem2();
+                        p2.Solve();
+                        break;
+                    // case "3":
+                    //     var p3 = new Problem3();
+                    //     p3.Solve();
+                    //     break;
+                    case "pf":
+                        var helper = new Helper();
+                        helper.WritePrimeFactorisation();
+                        break;
+                    case "e":
+                        exit = true;
+                        Console.WriteLine("Exitting");
+                        break;
+                    case "help":
+                        Console.WriteLine("Options: Problem 1 = 1, Problem 2 = 2, Prime Factorisation = pf, Exit =e");
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid option, write 'help' to see available options");
+                        break;
+                }
+                Console.WriteLine("---------------");
+            }
         }
     }
 }
